a7473c9d38c3f947dc08698db6a8fdec5e51bc77
Dev-JimmyZea26 <Alejandro.Zea@fundacion-jala.org>
2023-06-12 19:43:08 -0500

    feat: Add components folder and game.py file using pygame

---
+++ game/components/game.py
@@ -0,0 +1,58 @@
+import pygame as pg
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from game.components.spaceship import Spaceship
+
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption(TITLE)
+        pg.display.set_icon(ICON)
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pg.time.Clock()
+        self.playing = False
+        self.game_speed = 10
+        self.x_pos_bg = 0
+        self.y_pos_bg = 0
+        self.player = Spaceship()
+        
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        pg.display.quit()
+        pg.quit()
+        
+    # Events
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.playing = False
+                
+    # Update
+    def update(self):
+        user_input = pg.key.get_pressed()
+        self.player.update(user_input)
+    
+    # Draw
+    def draw(self):
+        self.clock.tick(FPS)
+        self.screen.fill((255, 255, 255))
+        self.draw_background()
+        self.player.draw(self.screen)
+        pg.display.update()
+        pg.display.flip()
+        
+    def draw_background(self):
+        image = pg.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        image_height = image.get_height()
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
+        
+        if self.y_pos_bg >= SCREEN_HEIGHT:
+            self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
+            self.y_pos_bg = 0
+        self.y_pos_bg += self.game_speed
+        
+    
