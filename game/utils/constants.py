55ac9ce190ad5cff9a9a768ded8b5d7ed278e5be
Dev-JimmyZea26 <Alejandro.Zea@fundacion-jala.org>
2023-06-15 10:20:29 -0500

    refactor: update constants.py leaving enemy constants separately

--- game/utils/constants.py
+++ game/utils/constants.py
@@ -23,12 +23,11 @@ SHIELD_TYPE = 'shield'
 SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
 SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship_shield.png"))
 BULLET = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_1.png"))
-
 BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_2.png"))
 
-_images_enemy_folder = os.listdir(os.path.join(IMG_DIR, "Enemy"))
-ENEMIES = []
-for _image in _images_enemy_folder:
-    ENEMIES.append(pygame.image.load(os.path.join(IMG_DIR, "Enemy", _image)))    
+ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
+ENEMY_2 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_2.png"))
+ENEMY_3 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_3.png"))
+ENEMY_4 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_4.png"))
 
 FONT_STYLE = 'freesansbold.ttf'
